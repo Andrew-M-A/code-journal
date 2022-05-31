20406b8493d0c023c499522c72c04bdad58cf271
Andrew Almeida <am.almeida000@gmail.com>
2022-05-31 04:29:45 +0000

    added submit eventlistener to form, put the form values into a new object, added nextEntryId and am incrememnting it

--- js/main.js
+++ js/main.js
@@ -11,6 +11,18 @@ $photoUrl.addEventListener('input', updateImg);
 function getEntries(event) {
   event.preventDefault();
 
+  var $entriesObj = {};
+
+  var $titleValue = document.getElementById('title-input').value;
+  var $urlValue = document.getElementById('photo-url').value;
+  var $notesValue = document.getElementById('user-notes').value;
+
+  $entriesObj.title = $titleValue;
+  $entriesObj.url = $urlValue;
+  $entriesObj.notes = $notesValue;
+  $entriesObj.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+
 }
 var $form = document.querySelector('form');
 $form.addEventListener('submit', getEntries);
