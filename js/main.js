69c5484426dfc76ca7f36414fb1db57a2582b836
Andrew Almeida <am.almeida000@gmail.com>
2022-06-01 00:41:24 +0000

    defined function that creates DOM tree

--- js/main.js
+++ js/main.js
@@ -33,3 +33,48 @@ function getEntries(event) {
   $form.reset();
 
 }
+
+function renderEntries(entry) {
+
+  var $rowDiv = document.createElement('div');
+  $rowDiv.setAttribute('class', 'row');
+
+  var $colHalfDiv = document.createElement('div');
+  $colHalfDiv.setAttribute('class', 'column-half');
+
+  $rowDiv.appendChild($colHalfDiv);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('src', ' ');
+
+  $colHalfDiv.appendChild($img);
+
+  var $colHalfDivTwo = document.createElement('div');
+  $colHalfDivTwo.setAttribute('class', 'column-half');
+
+  $rowDiv.appendChild($colHalfDivTwo);
+
+  var $ul = document.createElement('ul');
+  $colHalfDivTwo.appendChild($ul);
+
+  var $li1 = document.createElement('li');
+  var $li2 = document.createElement('li');
+  var $li3 = document.createElement('li');
+
+  $ul.appendChild($li1);
+  $ul.appendChild($li2);
+  $ul.appendChild($li3);
+
+  var $h2 = document.createElement('h2');
+  $li1.appendChild($h2);
+
+  var $p1 = document.createElement('p');
+  $li2.appendChild($p1);
+
+  var $p2 = document.createElement('p');
+  $li3.appendChild($p2);
+
+  return $rowDiv;
+}
+
+window.addEventListener('DOMContentLoaded', renderEntries);
